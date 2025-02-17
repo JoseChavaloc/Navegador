e8bff9f2744f36791b5abb7378738119bd26d3e7
chava <josechavaloc@umes.edu.gt>
2025-02-17 10:16:53 -0600

    Agregar archivos de proyecto.

---
+++ Navegador/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Navegador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.navegarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.haciaAtrasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.haciaDelanteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.btnIr = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.navegarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1089, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // navegarToolStripMenuItem
+            // 
+            this.navegarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inicioToolStripMenuItem,
+            this.haciaAtrasToolStripMenuItem,
+            this.haciaDelanteToolStripMenuItem});
+            this.navegarToolStripMenuItem.Name = "navegarToolStripMenuItem";
+            this.navegarToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.navegarToolStripMenuItem.Text = "Navegar";
+            // 
+            // inicioToolStripMenuItem
+            // 
+            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.inicioToolStripMenuItem.Text = "Inicio";
+            // 
+            // haciaAtrasToolStripMenuItem
+            // 
+            this.haciaAtrasToolStripMenuItem.Name = "haciaAtrasToolStripMenuItem";
+            this.haciaAtrasToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.haciaAtrasToolStripMenuItem.Text = "Hacia Atras";
+            // 
+            // haciaDelanteToolStripMenuItem
+            // 
+            this.haciaDelanteToolStripMenuItem.Name = "haciaDelanteToolStripMenuItem";
+            this.haciaDelanteToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.haciaDelanteToolStripMenuItem.Text = "Hacia delante";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "www.amazon.com",
+            "www.youtube.com",
+            "www.umes.edu.gt"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(294, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(312, 31);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(323, 24);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(641, 33);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(329, 22);
+            this.txtDireccion.TabIndex = 3;
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(0, 61);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(1089, 510);
+            this.webView21.TabIndex = 5;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // btnIr
+            // 
+            this.btnIr.Location = new System.Drawing.Point(976, 30);
+            this.btnIr.Name = "btnIr";
+            this.btnIr.Size = new System.Drawing.Size(107, 23);
+            this.btnIr.TabIndex = 6;
+            this.btnIr.Text = "Ir";
+            this.btnIr.UseVisualStyleBackColor = true;
+            this.btnIr.Click += new System.EventHandler(this.btnIr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 567);
+            this.Controls.Add(this.btnIr);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem navegarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem haciaAtrasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem haciaDelanteToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Button btnIr;
+    }
+}
+
---
+++ Navegador/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navegador
+{
+    public partial class Form1 : Form
+    {
+        private readonly string historialFile = @"../../historial.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            InicializarWebView();
+        }
+
+        public async void InicializarWebView()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+            webView21.Source = new Uri("https://www.google.com");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri("https://www.google.com");
+            CargarHistorial();
+
+        }
+
+        private void btnIr_Click(object sender, EventArgs e)
+        {
+            NavegarDesdeTextBox();
+
+        }
+
+        private void inicioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri("https://www.google.com");
+        }
+
+        private void haciaAtrásToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webView21.CanGoBack) webView21.GoBack();
+        }
+
+        private void haciaDelanteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webView21.CanGoForward) webView21.GoForward();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            ;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                string url = ObtenerURLValida(comboBox1.SelectedItem.ToString());
+                txtDireccion.Text = url;
+            }
+        }
+        private void NavegarDesdeTextBox()
+        {
+            string url = ObtenerURLValida(txtDireccion.Text);
+            webView21.Source = new Uri(url);
+            txtDireccion.Text = url;
+            Guardar(url);
+        }
+
+        private string ObtenerURLValida(string entrada)
+        {
+            entrada = entrada.Trim();
+
+            if (!entrada.Contains("."))
+            {
+                return "https://www.google.com/search?q=" + Uri.EscapeDataString(entrada);
+            }
+
+            if (!entrada.StartsWith("https://", StringComparison.OrdinalIgnoreCase) &&
+                !entrada.StartsWith("http://", StringComparison.OrdinalIgnoreCase))
+            {
+                entrada = "https://" + entrada;
+            }
+
+            return entrada;
+        }
+        private void Guardar(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url)) return;
+
+            List<string> historial = new List<string>();
+
+            // Leer historial si el archivo existe
+            if (File.Exists(historialFile))
+            {
+                historial = File.ReadAllLines(historialFile).ToList();
+            }
+
+            // Evitar duplicados y agregar nueva URL al inicio
+            historial.Remove(url);
+            historial.Insert(0, url);
+
+            // Mantener solo las últimas 10 entradas
+            if (historial.Count > 10)
+            {
+                historial = historial.Take(10).ToList();
+            }
+
+            // Guardar en el archivo
+            File.WriteAllLines(historialFile, historial);
+
+            // Refrescar historial en ComboBox
+            CargarHistorial();
+
+        }
+        private void CargarHistorial()
+        {
+            comboBox2.Items.Clear();
+
+            if (File.Exists(historialFile))
+            {
+                var historial = File.ReadAllLines(historialFile);
+                comboBox2.Items.AddRange(historial);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                string url = ObtenerURLValida(comboBox1.SelectedItem.ToString());
+                txtDireccion.Text = url;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
